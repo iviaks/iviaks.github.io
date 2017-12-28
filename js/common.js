6ef55c0abcee2fcf8561a6b121abd099d22acaa4
M. Saykov <max-saykov@mail.ru>
2017-12-28 13:24:35 +0200

    Truncate repo

--- js/common.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    var pgwSlider = $('.pgwSlider').pgwSlider();
-    pgwSlider.reload({
-        displayControls: true,
-        transitionEffect: 'sliding'
-    });
-});
